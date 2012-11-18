4603213ebe34b3b96f83df5166279b522c3dee1b
angeltop <antopalidou@hotmail.com>
2012-11-18 23:25:26 +0100

    class that describes the robot position a,y, theta

---
+++ src/RobotPosition.java
@@ -0,0 +1,34 @@
+
+public class RobotPosition {
+
+	private int x;
+	private int y;
+	private int theta;
+	
+	public RobotPosition(int x, int y, int theta){
+		this.x = x;
+		this.y = y;
+		this.theta = theta;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getTheta() {
+		return theta;
+	}
+	public void setTheta(int theta) {
+		this.theta = theta;
+	}
+	
+	
+}
