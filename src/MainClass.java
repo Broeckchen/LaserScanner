669ec672c24b3a2b7cc94e923a03733308932ad0
angeltop <antopalidou@hotmail.com>
2012-11-18 23:28:32 +0100

    generator call added in the main class

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,10 @@
 import java.awt.image.BufferedImage;
-import java.util.Random;
+import java.awt.image.Raster;
+import java.io.File;
+import java.io.IOException;
+
+
+import javax.imageio.ImageIO;
 
 
 public class MainClass {
@@ -9,18 +14,33 @@ public class MainClass {
 	 */
 	public static void main(String[] args) {
 
-		//generate a randomly filled measurement array;
-		double measurements[] = new double[55];
-		Random r = new Random();
-		for(int i=0; i<55;i++){
-			measurements[i] = r.nextDouble()*10;
-		}
-		for(double i : measurements){
-			System.out.println(i);
+		BufferedImage map;
+		try {
+			
+			
+			map = ImageIO.read(new File("ressources"+File.separator+"Assignment_5_Grid_Map.png"));
+			Raster matrix = map.getData();
+			
+			ModelParameters parameters  = new ModelParameters(10, 5, 135, 5, 1, 2, 0.1 );
+			
+			double[] measurements = new double[parameters.getAngleRange()/parameters.getAngleStep()+1]; 
+			RobotPosition position = new RobotPosition(10, 10, 0); //define a position for the robot
+			MeasurementGenerator generator = new MeasurementGenerator(matrix, position, parameters);
+			
+			// The computed measurements for the given robot position
+			measurements = generator.computeMeasurements();
+			
+			for(double i : measurements){
+				System.out.println(i);
+			}
+			
+			//use measurement to create a map of estimated positions
+			PositionFinder estimator = new PositionFinder();
+			BufferedImage likelihoodmap = estimator.generateMap(measurements);
+		} catch (IOException e) {
+
+			e.printStackTrace();
 		}
 		
-		//use measurement to create a map of estimated positions
-		PositionFinder estimator = new PositionFinder();
-		BufferedImage likelihoodmap = estimator.generateMap(measurements);
 	}
 }
